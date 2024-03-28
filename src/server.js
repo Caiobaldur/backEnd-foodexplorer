6d3466430054b13adc7436ded2b0966521eda99c
Caio Balduino <caiobrafachino@hotmail.com>
2024-03-28 16:28:01 -0300

    feat: Responsibilities Separation - Routes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
-const app = express();
+const routes = require("./routes")
 
-app.get("/", (req,res) => {
-  res.send('hello world')
-})
+const app = express();
+app.use(express.json())
+
+app.use(routes)
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
