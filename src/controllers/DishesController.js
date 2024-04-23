5376d679bda1d66c1a237d177e644aed3c2a3721
Caio Balduino <caiobrafachino@hotmail.com>
2024-04-23 18:49:41 -0300

    feat: finish dishcontroller

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -59,7 +59,61 @@ class DishesController {
     }
   }
 
-  
+  async show(req, res) {
+    const {id} = req.params
+    try {
+      const dish = await knex.select('*').from('dishes').where('id', id).first()
+      const ingredients = await knex('ingredients').where('dish_id', id)
+      const dishesWithIngredients = {dish, ingredients}
+      return res.status(200).json(dishesWithIngredients)
+    } catch(error) {
+      AppError("Não foi possível buscar prato", error)
+      return res.status(500).json({message:"Não foi possível buscar prato"})
+    }
+  }
+
+  async index(req, res) {
+    const { search } = req.query
+
+    try {
+      let dishesQuery = knex('dishes')
+      .select([
+        'dishes.id',
+        'dishes.name',
+        'dishes.price',
+        'dishes.description',
+        'dishes.image',
+        'dishes.category',
+        'dishes.created_at',
+      ])
+      .distinct()
+      .orderBy('dishes.name')
+
+      if(search) {
+        dishesQuery = dishesQuery
+        .where('dishes.name', 'like', `%${search}%`)
+        .orWhere('ingredients.name', 'like', `%${search}%`)
+        .join('ingredients', 'dishes.id', '=', 'ingredients.dish_id')
+      }
+
+      const dishes = await dishesQuery
+      return res.status(200).json(dishes)
+    } catch(error){
+      AppError("Não foi possível buscar pratos", error)
+      return res.status(500).json({message: "Não foi possível buscar pratos"})
+    }
+  }
+
+  async delete(req,res) {
+    try {
+      const {id} = req.params
+      await knex('dishes').where({id}).delete()
+      return res.status(202).json({message: "O prato foi excluído!"})
+    }catch(error) {
+      AppError("Não foi possível excluir prato:", error)
+      return res.status(500).json({message: "Não foi possível excluir prato"})
+    }
+  }
 }
 
 module.exports = DishesController;
