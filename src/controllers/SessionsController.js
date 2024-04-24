0c4c6b5394848a94554ee83f039e290305f7781a
Caio Balduino <caiobrafachino@hotmail.com>
2024-04-24 15:12:19 -0300

    feat: validating user db

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,6 +1,18 @@
+const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
+const { knex } = require("../database/knex");
+
+
 class SessionsController {
   async create(req, res){
     const { email, password } = req.body
+
+    const user = await knex("users").where({email}).first()
+
+    if(!user) {
+      throw new AppError("E-mail e/ou senha incorreto!", 401)
+    }
+
     return response.json({email, password})
   }
 
