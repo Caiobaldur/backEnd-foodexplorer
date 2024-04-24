94691cf5a6bf5ed7c862ad8ff9d8c782bc283d9f
Caio Balduino <caiobrafachino@hotmail.com>
2024-04-23 21:00:19 -0300

    refactor: unusable controller delete, combining functionalities in userscontroller

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -2,76 +2,89 @@ const { hash, compare } = require("bcrypt");
 const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
 
-
 class UsersController {
   async create(req, res) {
-    const { name, email, password, isAdmin } = req.body;
+    const { name, email, password } = req.body;
 
-    if(!name){
-      throw new AppError("Nome obrigatório!")
+    if (!name) {
+      throw new AppError("Nome obrigatório!");
     }
 
     const database = await sqliteConnection();
-    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
-    
-    if(checkUserExists){
-      throw new AppError("este e-mail já está em uso.")
+    const checkUserExists = await database.get("SELECT * FROM users");
+
+    let isAdmin = false;
+
+    if (!checkUserExists) {
+      isAdmin = true;
     }
-    
-    const hashedPassword = await hash(password,8);
-    
-    await database.run("INSERT INTO users (name,email,password) VALUES (?,?,?)", [name,email,hashedPassword])
-    
-    return res.status(201).json({ name, email, password });
+
+    const hashedPassword = await hash(password, 8);
+
+    await database.run("INSERT INTO users (name, email, password, isAdmin) VALUES (?, ?, ?, ?)", [
+      name,
+      email,
+      hashedPassword,
+      isAdmin ? 1 : 0,
+    ]);
+
+    // Verifica se o usuário recém-criado é administrador
+    const user = await database.get("SELECT * FROM users WHERE email = ?", [email]);
+    const roleId = isAdmin ? 1 : 2; // 1 para admin, 2 para customer
+
+    // Associa o usuário à role correspondente na tabela 'roles'
+    await database.run("INSERT INTO roles (user_id, role) VALUES (?, ?)", [user.id, roleId]);
+
+    return res.status(201).json({ name, email, isAdmin });
   }
 
-  async update(req, res){
-    const {name, email, password, old_password} = req.body
-    const {id} = req.params
+  async update(req, res) {
+    const { name, email, password, old_password } = req.body;
+    const { id } = req.params;
 
     const database = await sqliteConnection();
-    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
 
-    if(!user){
+    if (!user) {
       throw new AppError("Usuário não encontrado");
     }
 
-    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?) AND id != ?", [email, id]);
 
-    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
-      throw new AppError("Usuário já cadastrado!")
+    if (userWithUpdatedEmail) {
+      throw new AppError("E-mail já cadastrado!");
     }
 
     user.name = name ?? user.name;
     user.email = email ?? user.email;
 
-    if(password && !old_password){
-      throw new AppError("Informe a senha antiga!")
+    if (password && !old_password) {
+      throw new AppError("Informe a senha antiga!");
     }
-    
-    if(password && old_password){
+
+    if (password && old_password) {
       const checkOldPassword = await compare(old_password, user.password);
 
       if (!checkOldPassword) {
-        throw new AppError("A senha não confere.");
+        throw new AppError("A senha antiga não confere.");
       }
 
-      user.password = await hash(password, 8)
+      user.password = await hash(password, 8);
     }
 
-    await database.run(`
+    await database.run(
+      `
       UPDATE users SET
       name = ?,
       email = ?,
       password = ?,
       updated_at = DATETIME('now')
       WHERE id = ?`,
-      [user.name, user.email,user.password, id]
-    )
+      [user.name, user.email, user.password, id]
+    );
 
     return res.status(200).json();
   }
-  
 }
 
 module.exports = UsersController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,5 @@ const dishesRoutes = require("./dishes.routes.js");
 
 routes.use("/users", usersRouter);
 routes.use("/dishes", dishesRoutes);
-routes.use("/roles", rolesRoutes);
 
 module.exports = routes;
